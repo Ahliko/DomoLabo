ecaa7fbf0e0626c8a93d28116124f6df8748fb96
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-05-21 21:20:43 +0200

    add btn detection on fan

--- Connected_objects/Fan/main.py
+++ Connected_objects/Fan/main.py
@@ -1,16 +1,24 @@
 from simple import MQTTClient
 from BLEModule import BLE
-from Ventilo import Ventilo
+from ventilo import Ventilo
 import uasyncio as asyncio
+from machine import Pin
+
+button = Pin(14, Pin.IN, Pin.PULL_UP)
 
 async def main():
+    
+    while button.value():
+        pass
+    
+    print("btn pressed")
     ble = BLE("DL Object")
 
     while (not ble.connect) or (not Ventilo.wifi_ssid or not Ventilo.wifi_password):
         pass
 
     print(Ventilo.wifi_ssid + "  :  " + Ventilo.wifi_password)
-    
+
     ventilo = Ventilo()
     ventilo.main()
 
